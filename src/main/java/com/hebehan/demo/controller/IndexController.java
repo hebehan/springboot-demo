99ed20fe1a9e3f42d265256ef8f9bd0fb71d1937
Hebe <doveskila@gmail.com>
2017-11-06 19:14:28 +0800

    1

--- src/main/java/com/hebehan/demo/controller/IndexController.java
+++ src/main/java/com/hebehan/demo/controller/IndexController.java
@@ -6,6 +6,7 @@ import com.hebehan.demo.model.Result;
 import com.hebehan.demo.result.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,8 @@ public class IndexController {
     private PersonRepository personRepository;
 
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("user","hahhaah");
         return "index";
     }
 
